4a6e44282148bc1840073c8ac4788abc7987f762
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-07-28 17:42:43 -0700

    write_handlers.c commit

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 /**************START OF GUARD**********/
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef "MAIN_H"
+#define "MAIN.H"
 
 
 /******** STDLIB.H FILES TO INCLUDE******/
@@ -18,32 +18,27 @@
 #define UNUSED(m) (void)(m)
 
 
-#define PRINTF_BUFF_SIZE 1024
+#define BUFF_SIZE 1024
 
-int find_flgs(const char *format, int *index);
-int g_width(const char *format, int *index, va_list lst_prnt);
-int g_precision(const char *format, int *index, va_list lst_prnt);
-int g_size(const char *format, int *index, va_list lst_prnt);
 
 /************PROTOTYPES***********/
 int _printf(const char *format, ...);
-int g_prnt(const char *format, int *index, va_list lst_prnt, char buf[], int flags, int width, int precision, int size);
+int g_prnt(const char *format, int &index, va_list lst_prnt, char buf, int flags, int width, int precision, int size);
 int display_char(va_list clas, char buf[], int flags, int width, int precision, int size);
-int display_write(char v_cls, char buf[], int flags, int width, int precision, int size);
-int display_str(va_list clas, char buf[], int flags, int width, int precision, int size);
-long int csn(long int li, int size);
-int display_integer(va_list clas, char buf[], int flags, int width, int precision, int size);
-int write_integer(int neg, int index, char buf[], int flags, int width, int recision, int size);
+int display_str(va_list clas, char buf[], int flags, int width, int precision int size);
+int display_integer(va_list clas, char buf[], int flags, int width, int size);
 int display_unsg_octal(va_list clas, char buf[], int flags, int width, int precision, int size);
-int write_unsinteger(va_list clas, int index, char buf[], int flags, int width, int precision, int size);
 int display_percent(va_list clas, char buf[], int flags, int width, int precision, int size);
 int diaplay_unsigned_integer(va_list clas, char buf[], int flags, int width, int precision, int size);
 int display_unsgn_upprhex(va_list clas, char buf[], int flags, int width, int precision, int size);
-int write_hexdecim(va_list clas, const char *p, char buf[], int flags, int width, int precision, int size);
-int display_lowupper_hexa(va_list clas, char hold[], char buf[], int flags, int get_flagch, int width, int precision, int size);
+int display_lowupper_hexa(va_list clas, char hold, char buf[], int flags, int get_flagch, int precision, int size);
 int display_binary(va_list clas, char buf[], int flags, int width, int precision, int size);
 int display_hexadecimal(va_list clas, char buf[], int flags, int width, int precision, int size);
-
+int write_char_handler(char c, char buffer[], int flags, int width, int precision, int size);
+int write_number(int is_pos, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_numb(int ind, char buff[], int flags, int width, int precision, int length, char extra_c, char padd);
+int write_pointer(int ind, char buffer[], int length, int width, int flags, char padd, char extra_c, int padd_start);
+int write_unsg(int is_neg, int ind, char buffer[], int flags, int width, int precision, int size); 
 
 /********END OF GUARD*********/
 #endif
